917effa43a24c42f72602a66103886b0c31c9ece
ohmkub0 <phuwapat47@gmail.com>
2025-12-17 10:19:58 +0700

    1

--- 100pattern/5-chapter/index.js
+++ 100pattern/5-chapter/index.js
@@ -1,5 +1,5 @@
 function fetchData() {
-    return fetch('')
+    return fetch('https://jsonplaceholder.typicode.com/posts/1')
     .then(response => response.json())
     .then(data => console.log(data))
     .catch(error => console.error(error));
@@ -8,7 +8,7 @@ await fetchData2();
 
 async function fetchData2() { 
     try {
-    const response = await fetch('http://jsoonplacrholder.tpicode.com/posts/l');
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts/1');
     const data = await response.json();
     consoel.log(data);
     } catch (error) {
@@ -16,3 +16,7 @@ async function fetchData2() {
     }
 }
 await fetchData2();
+
+const response = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+const data = await response.json();
+console.log(data);
---
+++ 100pattern/ct6/index.js
@@ -0,0 +1,11 @@
+/* const [posts, users, randomuser] = await Promise.all([
+    fetch('https://jsonplaceholder.typicode.com/posts/1').then(response => response.json()),
+    fetch('https://jsonplaceholder.typicode.com/users/2').then(response => response.json()),
+    fetch('https://randomuser.me/api/?results=3').then(response => response.json())
+]);
+console.log(posts);
+console.log(users);
+console.log(randomuser); */
+
+
+
