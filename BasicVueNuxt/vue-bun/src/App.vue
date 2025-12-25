fa1501382ecfb9cc44cbfc43531554b58bb428e1
ohmkub0 <phuwapat47@gmail.com>
2025-12-25 16:48:35 +0700

    1

--- BasicVueNuxt/vue-bun/src/App.vue
+++ BasicVueNuxt/vue-bun/src/App.vue
@@ -1,14 +1,24 @@
-<template>
-  <p>{{ user , count }}</p>
-</template>
-
 <script setup lang="ts">
-import {ref, reactive} from 'vue'
+import { ref, reactive, computed } from 'vue'
 
-const count =ref(0)
-const user = reactive({name: 'a', age:20})
+const count = ref(0)
+
+const user = reactive({
+  name: 'john',
+  age: 20
+})
+const items = ref([1, 2, 3, 4])
+
+const double = computed(() => count.value * 2)
 </script>
 
-<style scoped>
+<template>
+  <h1>Counter: {{ count }}</h1>
+  <button @click="count++">+</button>
+  <button @click="count = 0">reset</button>
 
-</style>
+  <h1>{{user.name }}</h1>
+ 
+    <li v-for="item in items":key="item">{{ item }}</li>
+  <style scoped></style>
+</template>
